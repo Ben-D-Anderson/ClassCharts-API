6016fd4626fd923093665639b3d6fc71777c5bf0
Ben-D-Anderson <benanderson.david@gmail.com>
2020-11-22 17:37:16 +0000

    Lighter implementation of headers

--- src/main/java/com/terraboxstudios/classchartsapi/http/HttpResponse.java
+++ src/main/java/com/terraboxstudios/classchartsapi/http/HttpResponse.java
@@ -6,7 +6,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,26 +14,14 @@ public class HttpResponse {
 
     private final String content, responseMessage;
     private final int responseCode;
-    private final Map<String, String> responseHeaders;
+    private final Map<String, List<String>> responseHeaders;
 
 
     protected HttpResponse(HttpURLConnection httpURLConnection) throws IOException {
-
         this.responseMessage = httpURLConnection.getResponseMessage();
         this.responseCode = httpURLConnection.getResponseCode();
 
-        Map<String, String> responseHeaders = new HashMap<String, String>();
-        for (Map.Entry<String, List<String>> entries : httpURLConnection.getHeaderFields().entrySet()) {
-            StringBuilder values = new StringBuilder();
-            if (entries.getKey() == null) {
-                continue;
-            }
-            for (String value : entries.getValue()) {
-                values.append(value).append(",");
-            }
-            responseHeaders.put(entries.getKey(), values.toString());
-        }
-        this.responseHeaders = responseHeaders;
+        this.responseHeaders = httpURLConnection.getHeaderFields();
 
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(httpURLConnection.getInputStream()));
