6a728104584035162a61771edfe98a6c67c16dbd
Ben-D-Anderson <benanderson.david@gmail.com>
2020-11-23 23:08:56 +0000

    Using new DateValidator class

--- src/main/java/com/terraboxstudios/classchartsapi/StudentCredentials.java
+++ src/main/java/com/terraboxstudios/classchartsapi/StudentCredentials.java
@@ -4,31 +4,30 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.terraboxstudios.classchartsapi.exception.CodeException;
 import com.terraboxstudios.classchartsapi.exception.DateFormatException;
+import com.terraboxstudios.classchartsapi.exception.ServerException;
 import com.terraboxstudios.classchartsapi.http.HttpRequest;
 import com.terraboxstudios.classchartsapi.http.HttpResponse;
+import com.terraboxstudios.classchartsapi.utils.DateValidator;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
 import java.io.IOException;
 
+@Getter
 @AllArgsConstructor (access = AccessLevel.PRIVATE)
 public final class StudentCredentials {
 
-    @Getter
     private final String code;
-    @Getter
     private final String dateOfBirth;
-    private final static String dateRegex = "^\\d{2}/\\d{2}/\\d{4}$";
 
-    public static StudentCredentials from(String code, String dateOfBirth) throws DateFormatException, CodeException, IOException {
+    public static StudentCredentials from(String code, String dateOfBirth) throws DateFormatException, CodeException, IOException, ServerException {
         HttpRequest httpRequest = new HttpRequest.Builder
                 ("https://www.classcharts.com/student/checkpupilcode/" + code, "POST")
                 .build();
         HttpResponse httpResponse = httpRequest.execute();
         JsonObject jsonObject = JsonParser.parseString(httpResponse.getContent()).getAsJsonObject();
         if (jsonObject.get("success").getAsInt() != 1 || !jsonObject.get("data").getAsJsonObject().get("has_dob").getAsBoolean()) throw new CodeException("No user with a date of birth found for that code.");
-        if (!dateOfBirth.matches(dateRegex)) throw new DateFormatException("Date of birth provided does not follow format dd/MM/yyyy");
+        if (!DateValidator.isDateValid(dateOfBirth)) throw new DateFormatException("Date of birth provided does not follow format dd/MM/yyyy");
         return new StudentCredentials(code, dateOfBirth);
     }
 
