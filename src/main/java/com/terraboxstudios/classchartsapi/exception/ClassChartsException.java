f0c3f876a86dd71a8abab74789c4b4a63468f255
Ben-D-Anderson <benanderson.david@gmail.com>
2020-11-30 16:43:32 +0000

    Added ClassChartsException

---
+++ src/main/java/com/terraboxstudios/classchartsapi/exception/ClassChartsException.java
@@ -0,0 +1,9 @@
+package com.terraboxstudios.classchartsapi.exception;
+
+public class ClassChartsException extends Exception {
+
+    public ClassChartsException(String s) {
+        super(s);
+    }
+
+}
