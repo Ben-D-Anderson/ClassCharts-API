2e3b861bbac5e4386b79b917d76109379e22d8db
Ben-D-Anderson <benanderson.david@gmail.com>
2020-11-30 16:46:53 +0000

    Moved to student package and now using ClassChartsDate for dates

--- src/main/java/com/terraboxstudios/classchartsapi/student/StudentCredentials.java
+++ src/main/java/com/terraboxstudios/classchartsapi/student/StudentCredentials.java
@@ -1,16 +1,16 @@
-package com.terraboxstudios.classchartsapi;
+package com.terraboxstudios.classchartsapi.student;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.terraboxstudios.classchartsapi.exception.CodeException;
-import com.terraboxstudios.classchartsapi.exception.DateFormatException;
 import com.terraboxstudios.classchartsapi.exception.ServerException;
 import com.terraboxstudios.classchartsapi.http.HttpRequest;
 import com.terraboxstudios.classchartsapi.http.HttpResponse;
-import com.terraboxstudios.classchartsapi.utils.DateValidator;
+import com.terraboxstudios.classchartsapi.obj.ClassChartsDate;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
 import java.io.IOException;
 
 @Getter
@@ -20,15 +20,14 @@ public final class StudentCredentials {
     private final String code;
     private final String dateOfBirth;
 
-    public static StudentCredentials from(String code, String dateOfBirth) throws DateFormatException, CodeException, IOException, ServerException {
+    public static StudentCredentials from(String code, ClassChartsDate classChartsDate) throws CodeException, IOException, ServerException {
         HttpRequest httpRequest = new HttpRequest.Builder
                 ("https://www.classcharts.com/student/checkpupilcode/" + code, "POST")
                 .build();
         HttpResponse httpResponse = httpRequest.execute();
         JsonObject jsonObject = JsonParser.parseString(httpResponse.getContent()).getAsJsonObject();
         if (jsonObject.get("success").getAsInt() != 1 || !jsonObject.get("data").getAsJsonObject().get("has_dob").getAsBoolean()) throw new CodeException("No user with a date of birth found for that code.");
-        if (!DateValidator.isDateValid(dateOfBirth)) throw new DateFormatException("Date of birth provided does not follow format dd/MM/yyyy");
-        return new StudentCredentials(code, dateOfBirth);
+        return new StudentCredentials(code, classChartsDate.getDate());
     }
 
 }
