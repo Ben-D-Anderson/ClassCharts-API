20fe0aa64bf99b4ece7d2ea3d9419054cd62fdb8
Ben-D-Anderson <benanderson.david@gmail.com>
2021-04-11 20:26:11 +0100

    Created exception

---
+++ src/main/java/com/terraboxstudios/classchartsapi/exception/TimetableRetrievalException.java
@@ -0,0 +1,9 @@
+package com.terraboxstudios.classchartsapi.exception;
+
+public class TimetableRetrievalException extends ClassChartsException {
+
+    public TimetableRetrievalException(String s) {
+        super(s);
+    }
+
+}
