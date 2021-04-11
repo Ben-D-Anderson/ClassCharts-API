f4c1cd1000cde48ca302f463bbde2432cffca0b6
Ben-D-Anderson <benanderson.david@gmail.com>
2021-04-11 20:02:40 +0100

    Added timetable date feature and regulated format more

--- src/main/java/com/terraboxstudios/classchartsapi/obj/ClassChartsDate.java
+++ src/main/java/com/terraboxstudios/classchartsapi/obj/ClassChartsDate.java
@@ -5,6 +5,7 @@ import com.terraboxstudios.classchartsapi.utils.DateValidator;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.SneakyThrows;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -14,18 +15,22 @@ public class ClassChartsDate {
 
     @Getter
     private final String date;
+    private static final SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
 
     public static ClassChartsDate from(String date) throws DateFormatException {
         if (DateValidator.isDateValid(date)) {
             return new ClassChartsDate(date);
         }
-        throw new DateFormatException("Date of birth provided does not follow format dd/MM/yyyy");
+        throw new DateFormatException("Date of birth provided does not follow format " + format.toPattern());
     }
 
     public static ClassChartsDate from(Date date) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
-        String formattedDate = simpleDateFormat.format(date);
-        return new ClassChartsDate(formattedDate);
+        return new ClassChartsDate(format.format(date));
+    }
+
+    @SneakyThrows
+    public String getTimetableDate() {
+        return new SimpleDateFormat("yyyy-MM-dd").format(format.parse(date));
     }
 
 }
